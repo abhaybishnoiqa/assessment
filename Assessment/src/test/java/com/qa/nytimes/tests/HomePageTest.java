97e5bc63ff9c12da05b1a8abcc72f407040c7f9b
abhaybishnoiqa <130959969+abhaybishnoiqa@users.noreply.github.com>
2024-07-18 17:30:26 +0530

    Update HomePageTest.java

--- Assessment/src/test/java/com/qa/nytimes/tests/HomePageTest.java
+++ Assessment/src/test/java/com/qa/nytimes/tests/HomePageTest.java
@@ -2,32 +2,25 @@ package com.qa.nytimes.tests;
 
 import com.qa.nytimes.base.BaseTest;
 import com.qa.nytimes.utils.Constants;
-import io.qameta.allure.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
-@Epic("Epic - 100  - New York Editorial Site")
-@Story("US 101 - Validating Functionalities")
 public class HomePageTest extends BaseTest {
 
     @Test
-    @Description("Login page title test....")
-    @Severity(SeverityLevel.NORMAL)
     public void homePageTitleTest() {
         String nyPageTitle = homePage.getHomePageTitle();
         Assert.assertEquals(nyPageTitle, Constants.HOME_PAGE_TITLE);
     }
+    
     @Test
-    @Description("Check for login button link....")
-    @Severity(SeverityLevel.CRITICAL)
     public void homeButtonLinkTest() {
         Assert.assertTrue(homePage.isLoginLinkExist());
     }
 
     @Test
-    @Description("Check for US news..")
     public void validatingUsNewsLinkTest(){
         homePage.clickOnUSNav();
         String usPageURL = homePage.getUSPageURL();
@@ -36,7 +29,6 @@ public class HomePageTest extends BaseTest {
 
 
     @Test
-    @Description("Validating the search functionality..")
     public void validateSearchFunctionalityTest(){
         homePage.clickOnSearchIcon();
         homePage.searchForNews();
@@ -45,7 +37,6 @@ public class HomePageTest extends BaseTest {
     }
 
     @Test
-    @Description("Validating footer existence.")
     public void validatingFooterTest(){
         homePage.scrollDownToFooter();
         List<String> footerHeadings = homePage.getFooterHeadings();
@@ -54,7 +45,6 @@ public class HomePageTest extends BaseTest {
 
 
     @Test
-    @Description("Validating language translation")
     public void validateLanguageTranslationTest(){
         String language = homePage.clickOnOtherLanguage();
         Assert.assertEquals(language,Constants.LANGUAGE_SELECTED);
